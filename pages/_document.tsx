8f1707fff82a9cee5ba7467bc0985c4adf6cd726
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-22 18:40:21 +0200

    Update _document.tsx
    Co-Authored-By: VictorHugoAguilarAguilar <37845751+vhaa1718@users.noreply.github.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,6 @@ import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/do
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
         const originalRenderPage = ctx.renderPage
-
         // Run the React rendering logic synchronously
         ctx.renderPage = () =>
             originalRenderPage({
@@ -12,13 +11,10 @@ class MyDocument extends Document {
                 // Useful for wrapping in a per-page basis
                 enhanceComponent: (Component) => Component,
             })
-
         // Run the parent `getInitialProps`, it now includes the custom `renderPage`
         const initialProps = await Document.getInitialProps(ctx)
-
         return initialProps
     }
-
     render() {
         return (
             <Html>
