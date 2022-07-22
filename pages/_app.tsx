ee0fae249c2e2e9b57b5347ccc82968754ee2391
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-22 14:33:29 +0200

    create styles basic
    Co-Authored-By: VictorHugoAguilarAguilar <37845751+vhaa1718@users.noreply.github.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@emotion/react'
+import { lightTheme } from '../themes'
+
+import '../styles/globals.css'
+import { CssBaseline } from '@mui/material'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
