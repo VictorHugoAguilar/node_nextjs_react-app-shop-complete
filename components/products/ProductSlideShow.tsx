6efb23c3688db267a233143fcf366778ba9e53aa
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-22 21:27:22 +0200

    Update ProductSlideShow.tsx
    Co-Authored-By: VictorHugoAguilarAguilar <37845751+vhaa1718@users.noreply.github.com>

--- components/products/ProductSlideShow.tsx
+++ components/products/ProductSlideShow.tsx
@@ -1,31 +1,36 @@
-import { FC } from "react";
-import { Slide } from "react-slideshow-image";
+import { FC } from 'react';
+import { Slide } from 'react-slideshow-image';
 
+import 'react-slideshow-image/dist/styles.css';
+import styles from './ProductSlideshow.module.css';
 
 interface Props {
-    images: string[];
+    images: string[]
 }
 
-export const ProductSlideShow: FC<Props> = ({ images }) => {
+export const ProductSlideshow: FC<Props> = ({ images }) => {
     return (
         <Slide
             easing="ease"
-            autoplay={true}
-            duration={3000}
-            indicators>
-            {images.map(image => {
-                const url = `/products/${image}`;
-                return (
-                    <div className="" key={image}>
-                        <div style={{
-                            backgroundImage: `url(${url})`,
-                            backgroundSize: 'cover',
-                        }}>
+            duration={7000}
+            indicators
+        >
+            {
+                images.map(image => {
+                    const url = `/products/${image}`;
+                    return (
+                        <div className={styles['each-slide']} key={image}>
+                            <div style={{
+                                backgroundImage: `url(${url})`,
+                                backgroundSize: 'cover'
+                            }}>
+                            </div>
                         </div>
-                    </div>
-                )
-            })
+                    )
+
+                })
             }
+
         </Slide>
     )
-} 
+}
