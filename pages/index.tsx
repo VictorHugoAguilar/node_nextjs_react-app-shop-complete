9e6ea97227e9072fc8a7b9e08f132673c110fcbd
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-22 17:13:09 +0200

    create temporal homepage
    Co-Authored-By: VictorHugoAguilarAguilar <37845751+vhaa1718@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import { Typography } from '@mui/material'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import { ShopLayout } from '../components/layout'
+import { initialData } from '../database/products'
 
 const HomePage: NextPage = () => {
   return (
@@ -11,6 +12,25 @@ const HomePage: NextPage = () => {
     >
       <Typography variant="h1" component='h1'>Tienda</Typography>
       <Typography variant="h2" sx={{ mb: 1 }} >Todos los productos</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map(product => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component='img'
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
+
     </ShopLayout>
   )
 }
