1c42611bfed8fc79a847a774b47392fcbe64f2cf
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-25 20:24:20 +0200

    seed products

--- models/Product.ts
+++ models/Product.ts
@@ -1,62 +1,45 @@
 import mongoose, { Schema, model, Model } from 'mongoose';
-import { IProduct } from "../interfaces";
+import { IProduct } from '../interfaces';
+
 
 const productSchema = new Schema({
-    description: {
-        type: String,
-        required: true
-    },
-    images: {
-        type: [String]
-    },
-    inStock: {
-        type: Number,
-        required: true,
-        default: 0
-    },
-    price: {
-        type: Number,
-        required: true,
-        default: 0
-    },
+    description: { type: String, required: true },
+    images: [{ type: String }],
+    inStock: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
     sizes: [{
         type: String,
         enum: {
-            values: ['S', 'M', 'L', 'XL', 'XXL'],
-            message: '{VALUE} is not a valid size'
+            values: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            message: '{VALUE} no es un tamaño válido'
         }
     }],
-    slug: {
-        type: String,
-        required: true
-    },
-    tags: [{
-        type: String
-    }],
-    title: {
-        type: String,
-        required: true
-    },
+    slug: { type: String, required: true, unique: true },
+    tags: [{ type: String }],
+    title: { type: String, required: true },
     type: {
         type: String,
         enum: {
             values: ['shirts', 'pants', 'hoodies', 'hats'],
-            message: '{VALUE} is not a valid type'
+            message: '{VALUE} no es un tipo válido'
         }
     },
     gender: {
         type: String,
         enum: {
             values: ['men', 'women', 'kid', 'unisex'],
-            message: '{VALUE} is not a valid gender'
+            message: '{VALUE} no es un genero válido'
         }
     }
 }, {
     timestamps: true
-})
+});
 
-// TODO: crear indice de Mongo
+
+productSchema.index({ title: 'text', tags: 'text' });
+
 
 const Product: Model<IProduct> = mongoose.models.Product || model('Product', productSchema);
+
 
 export default Product;
