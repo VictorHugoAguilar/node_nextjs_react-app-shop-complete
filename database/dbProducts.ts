ce06c6ceb92e48a438f461b0d831c9369d5b8898
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-27 09:20:04 +0200

    Update dbProducts.ts

--- database/dbProducts.ts
+++ database/dbProducts.ts
@@ -3,18 +3,20 @@ import { IProduct } from "../interfaces";
 import { Product } from "../models";
 
 export const getProductBySlug = async (slug: string): Promise<IProduct | null> => {
-
     await db.connect();
-
     const product = await Product.findOne({ slug }).lean();
-
     await db.disconnect();
-
     if (!product) {
         return null;
     }
-
     return JSON.parse(JSON.stringify(product));
-
-
+}
+interface ProductSlug {
+    slug: string;
+}
+export const getAllProductSlugs = async (): Promise<ProductSlug[]> => {
+    await db.connect();
+    const slugs = await Product.find().select('slug -_id').lean();
+    await db.disconnect();
+    return slugs;
 }
