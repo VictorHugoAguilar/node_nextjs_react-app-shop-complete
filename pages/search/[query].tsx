4076b03f29c1dfc6bea9b1c47f4f7fe20c7563c6
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-27 10:41:25 +0200

    Update [query].tsx

--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -1,5 +1,5 @@
 import type { GetServerSideProps, NextPage } from 'next'
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { useProducts } from '../../hooks';
 import { ShopLayout } from '../../components/layout';
 import { FullScreenLoading } from '../../components/ui';
@@ -9,10 +9,12 @@ import { IProduct } from '../../interfaces';
 
 
 interface Props {
-    products: IProduct[]
+    products: IProduct[],
+    foundProducts: boolean,
+    query: string,
 }
 
-const SearchPage: NextPage<Props> = ({ products }) => {
+const SearchPage: NextPage<Props> = ({ products, foundProducts, query }) => {
 
     return (
         <ShopLayout
@@ -20,11 +22,18 @@ const SearchPage: NextPage<Props> = ({ products }) => {
             pageDescription="Busca los mejores productos para tu tienda"
             imageFull="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
         >
-            <Typography variant="h1" component='h1'  >Buscar producto</Typography>
-            <Typography variant="h2" sx={{ mb: 1 }}   >Todos los productos</Typography>
-
+            <Typography variant="h1" component='h1' >Buscar producto</Typography>
+            {foundProducts
+                ? (<Typography variant="h2" sx={{ mb: 1 }} textTransform='capitalize' > Término: {query}</Typography>)
+                :
+                (
+                    <Box display='flex'>
+                        <Typography variant="h2" sx={{ mb: 1 }}> No se han encontrado productos para </Typography>
+                        <Typography variant="h2" sx={{ mb: 1 }} color='secondary'> : {query}</Typography>
+                    </Box>
+                )
+            }
             <ProductList products={products} />
-
         </ShopLayout>
     )
 }
@@ -42,12 +51,18 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     }
     // y no hay productos
     let products = await dbProducts.getProductsByTerm(query);
+    const foundProducts = products.length > 0;
 
-    // TODO: retornar un error si no hay productos
+    // retornar todos los productos
+    if (!foundProducts) {
+        products = await dbProducts.getAllProducts();
+    }
 
     return {
         props: {
-            products
+            products,
+            foundProducts,
+            query
         }
     }
 }
