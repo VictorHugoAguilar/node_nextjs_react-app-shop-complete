6627e4bef03ce37b100e8386d96daccdb52e4f95
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-28 14:48:17 +0200

    create seed data user

--- database/seed-data.ts
+++ database/seed-data.ts
@@ -1,3 +1,5 @@
+import bcrypt from 'bcryptjs';
+
 interface SeedProduct {
     description: string;
     images: string[];
@@ -15,10 +17,32 @@ type ValidSizes = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
 type ValidTypes = 'shirts' | 'pants' | 'hoodies' | 'hats';
 
 interface SeedData {
+    users: SeedUser[],
     products: SeedProduct[],
 }
 
+interface SeedUser {
+    name: string;
+    email: string;
+    password: string;
+    role: 'admin' | 'client';
+}
+
 export const initialData: SeedData = {
+    users: [
+        {
+            name: 'Victor Hugo Aguilar',
+            email: 'victor@correo.com',
+            password: bcrypt.hashSync('123456'),
+            role: 'admin'
+        },
+        {
+            name: 'Jose Hugo Aguilar',
+            email: 'jose@correo.com',
+            password: bcrypt.hashSync('123456'),
+            role: 'client'
+        },
+    ],
     products: [
         {
             description: "Introducing the Tesla Chill Collection. The Men’s Chill Crew Neck Sweatshirt has a premium, heavyweight exterior and soft fleece interior for comfort in any season. The sweatshirt features a subtle thermoplastic polyurethane T logo on the chest and a Tesla wordmark below the back collar. Made from 60% cotton and 40% recycled polyester.",
